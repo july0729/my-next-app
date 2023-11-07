29b512876d655c6dd22293fefa47842a1d240a08
july0729 <gaoyt0830@163.com>
2023-11-07 14:50:44 +0800

    build: 添加basetemplate组件并设置storybook

---
+++ src/components/template/base/BaseTemplate.mocks.ts
@@ -0,0 +1,9 @@
+import {IBaseTemplate} from './BaseTemplate';
+
+const base: IBaseTemplate = {
+  sampleTextProp: 'haidei shi ni  ',
+};
+
+export const mockBaseTemplateProps = {
+  base,
+};
---
+++ src/components/template/base/BaseTemplate.tsx
@@ -0,0 +1,12 @@
+import styles from './BaseTemplate.module.css';
+
+export interface IBaseTemplate {
+  sampleTextProp: string;
+}
+
+const BaseTemplate: React.FC<IBaseTemplate> = ({sampleTextProp}) => {
+  return <div className={styles.root}>{sampleTextProp}</div>;
+};
+
+export default BaseTemplate;
+
